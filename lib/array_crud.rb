748af547b84aee54d007255f01841bc2362ff92e
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-01 18:11:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["I", "am", "really", "learning"]
+array = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
